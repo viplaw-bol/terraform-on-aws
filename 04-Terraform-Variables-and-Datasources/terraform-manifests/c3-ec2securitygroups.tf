7c31a8bc73705e156e571b4ae4547597d3fedadf
viplaw-bol <132674064+viplaw-bol@users.noreply.github.com>
2024-04-18 13:44:13 +0530

    Update c3-ec2securitygroups.tf

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "vpc-ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.16.0.0/24"]
   }
 
   egress {
@@ -15,7 +15,7 @@ resource "aws_security_group" "vpc-ssh" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.16.0.0/24"]
   }
 
   tags = {
@@ -32,21 +32,21 @@ resource "aws_security_group" "vpc-web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.16.0.0/24"]
   }
   ingress {
     description = "Allow Port 443"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.16.0.0/24"]
   }  
   egress {
     description = "Allow all ip and ports outbound"    
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.16.0.0/24"]
   }
 
   tags = {
